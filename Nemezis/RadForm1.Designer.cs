2b33df36ff3f4f4618dba54a4fe7af5bcc09d7ae
Du'Islingr <alcatraz0890@live.com>
2016-05-14 20:14:29 -0400

    Preliminary design and partialy coded.

---
+++ Nemezis/RadForm1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace Nemezis
+{
+    partial class RadForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radRadioButton1 = new Telerik.WinControls.UI.RadRadioButton();
+            this.radGroupBox1 = new Telerik.WinControls.UI.RadGroupBox();
+            this.radRadioButton5 = new Telerik.WinControls.UI.RadRadioButton();
+            this.radRadioButton4 = new Telerik.WinControls.UI.RadRadioButton();
+            this.radRadioButton3 = new Telerik.WinControls.UI.RadRadioButton();
+            this.radRadioButton2 = new Telerik.WinControls.UI.RadRadioButton();
+            this.radTextBox1 = new Telerik.WinControls.UI.RadTextBox();
+            this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel2 = new Telerik.WinControls.UI.RadLabel();
+            this.radTextBox2 = new Telerik.WinControls.UI.RadTextBox();
+            this.radTextBox3 = new Telerik.WinControls.UI.RadTextBox();
+            this.radLabel3 = new Telerik.WinControls.UI.RadLabel();
+            this.radToggleSwitch1 = new Telerik.WinControls.UI.RadToggleSwitch();
+            this.radGroupBox2 = new Telerik.WinControls.UI.RadGroupBox();
+            this.radGroupBox3 = new Telerik.WinControls.UI.RadGroupBox();
+            this.radToggleSwitch2 = new Telerik.WinControls.UI.RadToggleSwitch();
+            this.radLabel5 = new Telerik.WinControls.UI.RadLabel();
+            this.radTextBox4 = new Telerik.WinControls.UI.RadTextBox();
+            this.radLabel4 = new Telerik.WinControls.UI.RadLabel();
+            this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            this.radButton2 = new Telerik.WinControls.UI.RadButton();
+            this.visualStudio2012DarkTheme1 = new Telerik.WinControls.Themes.VisualStudio2012DarkTheme();
+            this.radButton3 = new Telerik.WinControls.UI.RadButton();
+            ((System.ComponentModel.ISupportInitialize)(this.radRadioButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGroupBox1)).BeginInit();
+            this.radGroupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radRadioButton5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radRadioButton4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radRadioButton3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radRadioButton2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radToggleSwitch1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGroupBox2)).BeginInit();
+            this.radGroupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radGroupBox3)).BeginInit();
+            this.radGroupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radToggleSwitch2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radRadioButton1
+            // 
+            this.radRadioButton1.Location = new System.Drawing.Point(5, 21);
+            this.radRadioButton1.Name = "radRadioButton1";
+            this.radRadioButton1.Size = new System.Drawing.Size(56, 18);
+            this.radRadioButton1.TabIndex = 0;
+            this.radRadioButton1.Text = "ARMv7";
+            // 
+            // radGroupBox1
+            // 
+            this.radGroupBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this.radGroupBox1.Controls.Add(this.radRadioButton5);
+            this.radGroupBox1.Controls.Add(this.radRadioButton4);
+            this.radGroupBox1.Controls.Add(this.radRadioButton3);
+            this.radGroupBox1.Controls.Add(this.radRadioButton2);
+            this.radGroupBox1.Controls.Add(this.radRadioButton1);
+            this.radGroupBox1.HeaderText = "Architectures";
+            this.radGroupBox1.Location = new System.Drawing.Point(224, 87);
+            this.radGroupBox1.Name = "radGroupBox1";
+            this.radGroupBox1.Size = new System.Drawing.Size(90, 143);
+            this.radGroupBox1.TabIndex = 1;
+            this.radGroupBox1.Text = "Architectures";
+            // 
+            // radRadioButton5
+            // 
+            this.radRadioButton5.Location = new System.Drawing.Point(6, 120);
+            this.radRadioButton5.Name = "radRadioButton5";
+            this.radRadioButton5.Size = new System.Drawing.Size(54, 18);
+            this.radRadioButton5.TabIndex = 4;
+            this.radRadioButton5.Text = "x86_64";
+            // 
+            // radRadioButton4
+            // 
+            this.radRadioButton4.Location = new System.Drawing.Point(6, 96);
+            this.radRadioButton4.Name = "radRadioButton4";
+            this.radRadioButton4.Size = new System.Drawing.Size(38, 18);
+            this.radRadioButton4.TabIndex = 3;
+            this.radRadioButton4.Text = "x86";
+            // 
+            // radRadioButton3
+            // 
+            this.radRadioButton3.Location = new System.Drawing.Point(6, 71);
+            this.radRadioButton3.Name = "radRadioButton3";
+            this.radRadioButton3.Size = new System.Drawing.Size(57, 18);
+            this.radRadioButton3.TabIndex = 2;
+            this.radRadioButton3.Text = "ARM64";
+            // 
+            // radRadioButton2
+            // 
+            this.radRadioButton2.Location = new System.Drawing.Point(6, 46);
+            this.radRadioButton2.Name = "radRadioButton2";
+            this.radRadioButton2.Size = new System.Drawing.Size(55, 18);
+            this.radRadioButton2.TabIndex = 1;
+            this.radRadioButton2.Text = "Thumb";
+            // 
+            // radTextBox1
+            // 
+            this.radTextBox1.Location = new System.Drawing.Point(5, 45);
+            this.radTextBox1.Name = "radTextBox1";
+            this.radTextBox1.Size = new System.Drawing.Size(188, 20);
+            this.radTextBox1.TabIndex = 2;
+            // 
+            // radLabel1
+            // 
+            this.radLabel1.Location = new System.Drawing.Point(7, 21);
+            this.radLabel1.Name = "radLabel1";
+            this.radLabel1.Size = new System.Drawing.Size(25, 18);
+            this.radLabel1.TabIndex = 3;
+            this.radLabel1.Text = "Hex";
+            // 
+            // radLabel2
+            // 
+            this.radLabel2.Location = new System.Drawing.Point(7, 71);
+            this.radLabel2.Name = "radLabel2";
+            this.radLabel2.Size = new System.Drawing.Size(25, 18);
+            this.radLabel2.TabIndex = 4;
+            this.radLabel2.Text = "Hex";
+            // 
+            // radTextBox2
+            // 
+            this.radTextBox2.Location = new System.Drawing.Point(7, 95);
+            this.radTextBox2.Name = "radTextBox2";
+            this.radTextBox2.Size = new System.Drawing.Size(107, 20);
+            this.radTextBox2.TabIndex = 5;
+            // 
+            // radTextBox3
+            // 
+            this.radTextBox3.Location = new System.Drawing.Point(7, 45);
+            this.radTextBox3.Name = "radTextBox3";
+            this.radTextBox3.Size = new System.Drawing.Size(107, 20);
+            this.radTextBox3.TabIndex = 6;
+            // 
+            // radLabel3
+            // 
+            this.radLabel3.Location = new System.Drawing.Point(7, 21);
+            this.radLabel3.Name = "radLabel3";
+            this.radLabel3.Size = new System.Drawing.Size(54, 18);
+            this.radLabel3.TabIndex = 7;
+            this.radLabel3.Text = "Assembly";
+            // 
+            // radToggleSwitch1
+            // 
+            this.radToggleSwitch1.Location = new System.Drawing.Point(119, 95);
+            this.radToggleSwitch1.Name = "radToggleSwitch1";
+            this.radToggleSwitch1.OffText = "NORMAL";
+            this.radToggleSwitch1.OnText = "LLDB/GDB";
+            this.radToggleSwitch1.Size = new System.Drawing.Size(74, 20);
+            this.radToggleSwitch1.TabIndex = 9;
+            this.radToggleSwitch1.Text = "radToggleSwitch1";
+            this.radToggleSwitch1.ToggleStateMode = Telerik.WinControls.UI.ToggleStateMode.Click;
+            // 
+            // radGroupBox2
+            // 
+            this.radGroupBox2.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this.radGroupBox2.Controls.Add(this.radTextBox1);
+            this.radGroupBox2.Controls.Add(this.radTextBox2);
+            this.radGroupBox2.Controls.Add(this.radLabel3);
+            this.radGroupBox2.Controls.Add(this.radToggleSwitch1);
+            this.radGroupBox2.Controls.Add(this.radLabel2);
+            this.radGroupBox2.HeaderText = "Assembly to Hex";
+            this.radGroupBox2.Location = new System.Drawing.Point(12, 12);
+            this.radGroupBox2.Name = "radGroupBox2";
+            this.radGroupBox2.Size = new System.Drawing.Size(206, 121);
+            this.radGroupBox2.TabIndex = 10;
+            this.radGroupBox2.Text = "Assembly to Hex";
+            // 
+            // radGroupBox3
+            // 
+            this.radGroupBox3.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this.radGroupBox3.Controls.Add(this.radToggleSwitch2);
+            this.radGroupBox3.Controls.Add(this.radLabel5);
+            this.radGroupBox3.Controls.Add(this.radTextBox4);
+            this.radGroupBox3.Controls.Add(this.radLabel4);
+            this.radGroupBox3.Controls.Add(this.radLabel1);
+            this.radGroupBox3.Controls.Add(this.radTextBox3);
+            this.radGroupBox3.HeaderText = "Hex to Assembly";
+            this.radGroupBox3.Location = new System.Drawing.Point(12, 140);
+            this.radGroupBox3.Name = "radGroupBox3";
+            this.radGroupBox3.Size = new System.Drawing.Size(206, 121);
+            this.radGroupBox3.TabIndex = 11;
+            this.radGroupBox3.Text = "Hex to Assembly";
+            // 
+            // radToggleSwitch2
+            // 
+            this.radToggleSwitch2.Location = new System.Drawing.Point(119, 45);
+            this.radToggleSwitch2.Name = "radToggleSwitch2";
+            this.radToggleSwitch2.OffText = "Normal";
+            this.radToggleSwitch2.OnText = "LLDB/GDB";
+            this.radToggleSwitch2.Size = new System.Drawing.Size(73, 20);
+            this.radToggleSwitch2.TabIndex = 12;
+            this.radToggleSwitch2.Text = "radToggleSwitch2";
+            // 
+            // radLabel5
+            // 
+            this.radLabel5.Location = new System.Drawing.Point(7, 71);
+            this.radLabel5.Name = "radLabel5";
+            this.radLabel5.Size = new System.Drawing.Size(54, 18);
+            this.radLabel5.TabIndex = 11;
+            this.radLabel5.Text = "Assembly";
+            // 
+            // radTextBox4
+            // 
+            this.radTextBox4.Location = new System.Drawing.Point(7, 95);
+            this.radTextBox4.Name = "radTextBox4";
+            this.radTextBox4.Size = new System.Drawing.Size(187, 20);
+            this.radTextBox4.TabIndex = 10;
+            // 
+            // radLabel4
+            // 
+            this.radLabel4.Location = new System.Drawing.Point(-69, 82);
+            this.radLabel4.Name = "radLabel4";
+            this.radLabel4.Size = new System.Drawing.Size(55, 18);
+            this.radLabel4.TabIndex = 7;
+            this.radLabel4.Text = "radLabel4";
+            // 
+            // radButton1
+            // 
+            this.radButton1.Location = new System.Drawing.Point(224, 27);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(90, 24);
+            this.radButton1.TabIndex = 12;
+            this.radButton1.Text = "Convert Asm";
+            this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
+            // 
+            // radButton2
+            // 
+            this.radButton2.Location = new System.Drawing.Point(224, 57);
+            this.radButton2.Name = "radButton2";
+            this.radButton2.Size = new System.Drawing.Size(90, 24);
+            this.radButton2.TabIndex = 13;
+            this.radButton2.Text = "Convert Hex";
+            this.radButton2.Click += new System.EventHandler(this.radButton2_Click);
+            // 
+            // radButton3
+            // 
+            this.radButton3.Location = new System.Drawing.Point(224, 236);
+            this.radButton3.Name = "radButton3";
+            this.radButton3.Size = new System.Drawing.Size(90, 25);
+            this.radButton3.TabIndex = 14;
+            this.radButton3.Text = "About";
+            this.radButton3.Click += new System.EventHandler(this.radButton3_Click);
+            // 
+            // RadForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 268);
+            this.Controls.Add(this.radButton3);
+            this.Controls.Add(this.radButton2);
+            this.Controls.Add(this.radButton1);
+            this.Controls.Add(this.radGroupBox3);
+            this.Controls.Add(this.radGroupBox2);
+            this.Controls.Add(this.radGroupBox1);
+            this.Name = "RadForm1";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "Nemezis";
+            ((System.ComponentModel.ISupportInitialize)(this.radRadioButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGroupBox1)).EndInit();
+            this.radGroupBox1.ResumeLayout(false);
+            this.radGroupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radRadioButton5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radRadioButton4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radRadioButton3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radRadioButton2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radToggleSwitch1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGroupBox2)).EndInit();
+            this.radGroupBox2.ResumeLayout(false);
+            this.radGroupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radGroupBox3)).EndInit();
+            this.radGroupBox3.ResumeLayout(false);
+            this.radGroupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radToggleSwitch2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadRadioButton radRadioButton1;
+        private Telerik.WinControls.UI.RadGroupBox radGroupBox1;
+        private Telerik.WinControls.UI.RadRadioButton radRadioButton5;
+        private Telerik.WinControls.UI.RadRadioButton radRadioButton4;
+        private Telerik.WinControls.UI.RadRadioButton radRadioButton3;
+        private Telerik.WinControls.UI.RadRadioButton radRadioButton2;
+        private Telerik.WinControls.UI.RadTextBox radTextBox1;
+        private Telerik.WinControls.UI.RadLabel radLabel1;
+        private Telerik.WinControls.UI.RadLabel radLabel2;
+        private Telerik.WinControls.UI.RadTextBox radTextBox2;
+        private Telerik.WinControls.UI.RadTextBox radTextBox3;
+        private Telerik.WinControls.UI.RadLabel radLabel3;
+        private Telerik.WinControls.UI.RadToggleSwitch radToggleSwitch1;
+        private Telerik.WinControls.UI.RadGroupBox radGroupBox2;
+        private Telerik.WinControls.UI.RadGroupBox radGroupBox3;
+        private Telerik.WinControls.UI.RadToggleSwitch radToggleSwitch2;
+        private Telerik.WinControls.UI.RadLabel radLabel5;
+        private Telerik.WinControls.UI.RadTextBox radTextBox4;
+        private Telerik.WinControls.UI.RadLabel radLabel4;
+        private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.UI.RadButton radButton2;
+        private Telerik.WinControls.Themes.VisualStudio2012DarkTheme visualStudio2012DarkTheme1;
+        private Telerik.WinControls.UI.RadButton radButton3;
+    }
+}
---
+++ Nemezis/RadForm1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using Telerik.WinControls;
+using Telerik.WinControls.UI;
+
+namespace Nemezis
+{
+    public partial class RadForm1 : RadForm
+    {
+        public bool _CanRun = true;
+
+        string aParams = "";
+
+        private string _PathLLVMMCexe = Application.StartupPath + @"/files/llvm-mc.exe";
+
+        public RadForm1()
+        {
+            InitializeComponent();
+            ThemeResolutionService.ApplicationThemeName = "VisualStudio2012Dark";
+            string _LocalUserPath = Path.GetDirectoryName(Application.ExecutablePath);
+
+            CheckFiles();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RadTypeResolver.Instance.ResolveTypesInCurrentAssembly = true;
+        }
+
+        private void CheckFiles()
+        {
+            bool bNoLLVMMCExe = !File.Exists(_PathLLVMMCexe);
+            if (bNoLLVMMCExe)
+            {
+                string sFilesNotFound = "";
+                if (bNoLLVMMCExe)
+                    sFilesNotFound += "/files/llvm-mc.exe ";
+                string sMsg = string.Format(format: "Required file not found! ({0})", arg0: sFilesNotFound.Trim());
+                this.Hide();
+                RadMessageBox.Show(this, sMsg, caption: "Error", buttons: MessageBoxButtons.OK);
+                this._CanRun = false;
+            }
+        }
+
+        private void radButton1_Click(object sender, EventArgs e)
+        {
+            if (radRadioButton1.IsChecked)
+            {
+                aParams = "-arch=arm";
+            }
+            else if (radRadioButton2.IsChecked)
+            {
+                aParams = "-arch=thumb";
+            }
+            else if (radRadioButton3.IsChecked)
+            {
+                aParams = "-arch=aarch64";
+            }
+            else if (radRadioButton4.IsChecked)
+            {
+                aParams = "-arch=x86";
+            }
+            else if (radRadioButton5.IsChecked)
+            {
+                aParams = "-arch=x86-64";
+            }
+
+        }
+
+        private void radButton2_Click(object sender, EventArgs e)
+        {
+            if (radRadioButton1.IsChecked)
+            {
+                aParams = "-arch=arm";
+            }
+            else if (radRadioButton2.IsChecked)
+            {
+                aParams = "-arch=thumb";
+            }
+            else if (radRadioButton3.IsChecked)
+            {
+                aParams = "-arch=aarch64";
+            }
+            else if (radRadioButton4.IsChecked)
+            {
+                aParams = "-arch=x86";
+            }
+            else if (radRadioButton5.IsChecked)
+            {
+                aParams = "-arch=x86-64";
+            }
+        }
+
+        private void radButton3_Click(object sender, EventArgs e)
+        {
+            RadMessageBox.Show("Nemezis v1.0.0!\nDesigned by Du'Islingr", "About");
+        }
+    }
+}
