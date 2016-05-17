f31070a52cd97d01a7b1ec97fd0927aedb382bfd
DuIslingr <alcatraz0890@live.com>
2016-05-17 18:59:54 -0400

    Blizzard
    Mostly Functioning. Still needs some work though.

--- Nemezis/RadForm1.Designer.cs
+++ Nemezis/RadForm1.Designer.cs
@@ -28,17 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.radRadioButton1 = new Telerik.WinControls.UI.RadRadioButton();
+            this.radioArm = new Telerik.WinControls.UI.RadRadioButton();
             this.radGroupBox1 = new Telerik.WinControls.UI.RadGroupBox();
-            this.radRadioButton5 = new Telerik.WinControls.UI.RadRadioButton();
-            this.radRadioButton4 = new Telerik.WinControls.UI.RadRadioButton();
-            this.radRadioButton3 = new Telerik.WinControls.UI.RadRadioButton();
-            this.radRadioButton2 = new Telerik.WinControls.UI.RadRadioButton();
-            this.radTextBox1 = new Telerik.WinControls.UI.RadTextBox();
+            this.radioX86_64 = new Telerik.WinControls.UI.RadRadioButton();
+            this.radioX86 = new Telerik.WinControls.UI.RadRadioButton();
+            this.radioAarch64 = new Telerik.WinControls.UI.RadRadioButton();
+            this.radioThumb = new Telerik.WinControls.UI.RadRadioButton();
+            this.asmToHexAsmTextbox = new Telerik.WinControls.UI.RadTextBox();
             this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
             this.radLabel2 = new Telerik.WinControls.UI.RadLabel();
-            this.radTextBox2 = new Telerik.WinControls.UI.RadTextBox();
-            this.radTextBox3 = new Telerik.WinControls.UI.RadTextBox();
+            this.asmToHexHexTextBox = new Telerik.WinControls.UI.RadTextBox();
+            this.hexToAsmHexTextbox = new Telerik.WinControls.UI.RadTextBox();
             this.radLabel3 = new Telerik.WinControls.UI.RadLabel();
             this.radToggleSwitch1 = new Telerik.WinControls.UI.RadToggleSwitch();
             this.radGroupBox2 = new Telerik.WinControls.UI.RadGroupBox();
@@ -51,18 +51,18 @@
             this.radButton2 = new Telerik.WinControls.UI.RadButton();
             this.visualStudio2012DarkTheme1 = new Telerik.WinControls.Themes.VisualStudio2012DarkTheme();
             this.radButton3 = new Telerik.WinControls.UI.RadButton();
-            ((System.ComponentModel.ISupportInitialize)(this.radRadioButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radioArm)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radGroupBox1)).BeginInit();
             this.radGroupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.radRadioButton5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radRadioButton4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radRadioButton3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radRadioButton2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radioX86_64)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radioX86)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radioAarch64)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radioThumb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.asmToHexAsmTextbox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radTextBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radTextBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.asmToHexHexTextBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hexToAsmHexTextbox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radLabel3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radToggleSwitch1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radGroupBox2)).BeginInit();
@@ -79,22 +79,22 @@
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             this.SuspendLayout();
             // 
-            // radRadioButton1
+            // radioArm
             // 
-            this.radRadioButton1.Location = new System.Drawing.Point(5, 21);
-            this.radRadioButton1.Name = "radRadioButton1";
-            this.radRadioButton1.Size = new System.Drawing.Size(56, 18);
-            this.radRadioButton1.TabIndex = 0;
-            this.radRadioButton1.Text = "ARMv7";
+            this.radioArm.Location = new System.Drawing.Point(5, 21);
+            this.radioArm.Name = "radioArm";
+            this.radioArm.Size = new System.Drawing.Size(56, 18);
+            this.radioArm.TabIndex = 0;
+            this.radioArm.Text = "ARMv7";
             // 
             // radGroupBox1
             // 
             this.radGroupBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
-            this.radGroupBox1.Controls.Add(this.radRadioButton5);
-            this.radGroupBox1.Controls.Add(this.radRadioButton4);
-            this.radGroupBox1.Controls.Add(this.radRadioButton3);
-            this.radGroupBox1.Controls.Add(this.radRadioButton2);
-            this.radGroupBox1.Controls.Add(this.radRadioButton1);
+            this.radGroupBox1.Controls.Add(this.radioX86_64);
+            this.radGroupBox1.Controls.Add(this.radioX86);
+            this.radGroupBox1.Controls.Add(this.radioAarch64);
+            this.radGroupBox1.Controls.Add(this.radioThumb);
+            this.radGroupBox1.Controls.Add(this.radioArm);
             this.radGroupBox1.HeaderText = "Architectures";
             this.radGroupBox1.Location = new System.Drawing.Point(224, 87);
             this.radGroupBox1.Name = "radGroupBox1";
@@ -102,44 +102,44 @@
             this.radGroupBox1.TabIndex = 1;
             this.radGroupBox1.Text = "Architectures";
             // 
-            // radRadioButton5
+            // radioX86_64
             // 
-            this.radRadioButton5.Location = new System.Drawing.Point(6, 120);
-            this.radRadioButton5.Name = "radRadioButton5";
-            this.radRadioButton5.Size = new System.Drawing.Size(54, 18);
-            this.radRadioButton5.TabIndex = 4;
-            this.radRadioButton5.Text = "x86_64";
+            this.radioX86_64.Location = new System.Drawing.Point(6, 120);
+            this.radioX86_64.Name = "radioX86_64";
+            this.radioX86_64.Size = new System.Drawing.Size(54, 18);
+            this.radioX86_64.TabIndex = 4;
+            this.radioX86_64.Text = "x86_64";
             // 
-            // radRadioButton4
+            // radioX86
             // 
-            this.radRadioButton4.Location = new System.Drawing.Point(6, 96);
-            this.radRadioButton4.Name = "radRadioButton4";
-            this.radRadioButton4.Size = new System.Drawing.Size(38, 18);
-            this.radRadioButton4.TabIndex = 3;
-            this.radRadioButton4.Text = "x86";
+            this.radioX86.Location = new System.Drawing.Point(6, 96);
+            this.radioX86.Name = "radioX86";
+            this.radioX86.Size = new System.Drawing.Size(38, 18);
+            this.radioX86.TabIndex = 3;
+            this.radioX86.Text = "x86";
             // 
-            // radRadioButton3
+            // radioAarch64
             // 
-            this.radRadioButton3.Location = new System.Drawing.Point(6, 71);
-            this.radRadioButton3.Name = "radRadioButton3";
-            this.radRadioButton3.Size = new System.Drawing.Size(57, 18);
-            this.radRadioButton3.TabIndex = 2;
-            this.radRadioButton3.Text = "ARM64";
+            this.radioAarch64.Location = new System.Drawing.Point(6, 71);
+            this.radioAarch64.Name = "radioAarch64";
+            this.radioAarch64.Size = new System.Drawing.Size(57, 18);
+            this.radioAarch64.TabIndex = 2;
+            this.radioAarch64.Text = "ARM64";
             // 
-            // radRadioButton2
+            // radioThumb
             // 
-            this.radRadioButton2.Location = new System.Drawing.Point(6, 46);
-            this.radRadioButton2.Name = "radRadioButton2";
-            this.radRadioButton2.Size = new System.Drawing.Size(55, 18);
-            this.radRadioButton2.TabIndex = 1;
-            this.radRadioButton2.Text = "Thumb";
+            this.radioThumb.Location = new System.Drawing.Point(6, 46);
+            this.radioThumb.Name = "radioThumb";
+            this.radioThumb.Size = new System.Drawing.Size(55, 18);
+            this.radioThumb.TabIndex = 1;
+            this.radioThumb.Text = "Thumb";
             // 
-            // radTextBox1
+            // asmToHexAsmTextbox
             // 
-            this.radTextBox1.Location = new System.Drawing.Point(5, 45);
-            this.radTextBox1.Name = "radTextBox1";
-            this.radTextBox1.Size = new System.Drawing.Size(188, 20);
-            this.radTextBox1.TabIndex = 2;
+            this.asmToHexAsmTextbox.Location = new System.Drawing.Point(5, 45);
+            this.asmToHexAsmTextbox.Name = "asmToHexAsmTextbox";
+            this.asmToHexAsmTextbox.Size = new System.Drawing.Size(188, 20);
+            this.asmToHexAsmTextbox.TabIndex = 2;
             // 
             // radLabel1
             // 
@@ -157,19 +157,19 @@
             this.radLabel2.TabIndex = 4;
             this.radLabel2.Text = "Hex";
             // 
-            // radTextBox2
+            // asmToHexHexTextBox
             // 
-            this.radTextBox2.Location = new System.Drawing.Point(7, 95);
-            this.radTextBox2.Name = "radTextBox2";
-            this.radTextBox2.Size = new System.Drawing.Size(107, 20);
-            this.radTextBox2.TabIndex = 5;
+            this.asmToHexHexTextBox.Location = new System.Drawing.Point(7, 95);
+            this.asmToHexHexTextBox.Name = "asmToHexHexTextBox";
+            this.asmToHexHexTextBox.Size = new System.Drawing.Size(107, 20);
+            this.asmToHexHexTextBox.TabIndex = 5;
             // 
-            // radTextBox3
+            // hexToAsmHexTextbox
             // 
-            this.radTextBox3.Location = new System.Drawing.Point(7, 45);
-            this.radTextBox3.Name = "radTextBox3";
-            this.radTextBox3.Size = new System.Drawing.Size(107, 20);
-            this.radTextBox3.TabIndex = 6;
+            this.hexToAsmHexTextbox.Location = new System.Drawing.Point(7, 45);
+            this.hexToAsmHexTextbox.Name = "hexToAsmHexTextbox";
+            this.hexToAsmHexTextbox.Size = new System.Drawing.Size(107, 20);
+            this.hexToAsmHexTextbox.TabIndex = 6;
             // 
             // radLabel3
             // 
@@ -193,8 +193,8 @@
             // radGroupBox2
             // 
             this.radGroupBox2.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
-            this.radGroupBox2.Controls.Add(this.radTextBox1);
-            this.radGroupBox2.Controls.Add(this.radTextBox2);
+            this.radGroupBox2.Controls.Add(this.asmToHexAsmTextbox);
+            this.radGroupBox2.Controls.Add(this.asmToHexHexTextBox);
             this.radGroupBox2.Controls.Add(this.radLabel3);
             this.radGroupBox2.Controls.Add(this.radToggleSwitch1);
             this.radGroupBox2.Controls.Add(this.radLabel2);
@@ -213,7 +213,7 @@
             this.radGroupBox3.Controls.Add(this.radTextBox4);
             this.radGroupBox3.Controls.Add(this.radLabel4);
             this.radGroupBox3.Controls.Add(this.radLabel1);
-            this.radGroupBox3.Controls.Add(this.radTextBox3);
+            this.radGroupBox3.Controls.Add(this.hexToAsmHexTextbox);
             this.radGroupBox3.HeaderText = "Hex to Assembly";
             this.radGroupBox3.Location = new System.Drawing.Point(12, 140);
             this.radGroupBox3.Name = "radGroupBox3";
@@ -261,7 +261,7 @@
             this.radButton1.Size = new System.Drawing.Size(90, 24);
             this.radButton1.TabIndex = 12;
             this.radButton1.Text = "Convert Asm";
-            this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
+            this.radButton1.Click += new System.EventHandler(this.convertToAsm_Click);
             // 
             // radButton2
             // 
@@ -270,7 +270,7 @@
             this.radButton2.Size = new System.Drawing.Size(90, 24);
             this.radButton2.TabIndex = 13;
             this.radButton2.Text = "Convert Hex";
-            this.radButton2.Click += new System.EventHandler(this.radButton2_Click);
+            this.radButton2.Click += new System.EventHandler(this.convertToHex_Click);
             // 
             // radButton3
             // 
@@ -300,19 +300,19 @@
             // 
             this.RootElement.ApplyShapeToControl = true;
             this.Text = "Nemezis";
-            ((System.ComponentModel.ISupportInitialize)(this.radRadioButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radioArm)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.radGroupBox1)).EndInit();
             this.radGroupBox1.ResumeLayout(false);
             this.radGroupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.radRadioButton5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radRadioButton4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radRadioButton3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radRadioButton2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radioX86_64)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radioX86)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radioAarch64)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radioThumb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.asmToHexAsmTextbox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radTextBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radTextBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.asmToHexHexTextBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hexToAsmHexTextbox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.radLabel3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.radToggleSwitch1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.radGroupBox2)).EndInit();
@@ -335,17 +335,17 @@
 
         #endregion
 
-        private Telerik.WinControls.UI.RadRadioButton radRadioButton1;
+        private Telerik.WinControls.UI.RadRadioButton radioArm;
         private Telerik.WinControls.UI.RadGroupBox radGroupBox1;
-        private Telerik.WinControls.UI.RadRadioButton radRadioButton5;
-        private Telerik.WinControls.UI.RadRadioButton radRadioButton4;
-        private Telerik.WinControls.UI.RadRadioButton radRadioButton3;
-        private Telerik.WinControls.UI.RadRadioButton radRadioButton2;
-        private Telerik.WinControls.UI.RadTextBox radTextBox1;
+        private Telerik.WinControls.UI.RadRadioButton radioX86_64;
+        private Telerik.WinControls.UI.RadRadioButton radioX86;
+        private Telerik.WinControls.UI.RadRadioButton radioAarch64;
+        private Telerik.WinControls.UI.RadRadioButton radioThumb;
+        private Telerik.WinControls.UI.RadTextBox asmToHexAsmTextbox;
         private Telerik.WinControls.UI.RadLabel radLabel1;
         private Telerik.WinControls.UI.RadLabel radLabel2;
-        private Telerik.WinControls.UI.RadTextBox radTextBox2;
-        private Telerik.WinControls.UI.RadTextBox radTextBox3;
+        private Telerik.WinControls.UI.RadTextBox asmToHexHexTextBox;
+        private Telerik.WinControls.UI.RadTextBox hexToAsmHexTextbox;
         private Telerik.WinControls.UI.RadLabel radLabel3;
         private Telerik.WinControls.UI.RadToggleSwitch radToggleSwitch1;
         private Telerik.WinControls.UI.RadGroupBox radGroupBox2;
--- Nemezis/RadForm1.cs
+++ Nemezis/RadForm1.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.ComponentModel;
+using System.Collections.Generic;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
 using Telerik.WinControls;
@@ -13,9 +12,15 @@ namespace Nemezis
     {
         public bool _CanRun = true;
 
-        string aParams = "";
+        private string _FilesFolderPath = Application.StartupPath + "/files/";
+        private string _PathLLVMMCexe { get { return Path.Combine(_FilesFolderPath, "llvm-mc.exe"); } }
+        private string _PathInputFile { get { return Path.Combine(_FilesFolderPath, "tmpFile"); } }
 
-        private string _PathLLVMMCexe = Application.StartupPath + @"/files/llvm-mc.exe";
+        private string _LlvmExecuteCommand = " \"{0}\" {1} -show-encoding";
+
+        private delegate void receivedConsoleResponseDelegate(string output);
+
+        private string consoleResponse = "";
 
         public RadForm1()
         {
@@ -33,66 +38,163 @@ namespace Nemezis
 
         private void CheckFiles()
         {
-            bool bNoLLVMMCExe = !File.Exists(_PathLLVMMCexe);
+            string pathToLLvmExe = Path.Combine(_FilesFolderPath, _PathLLVMMCexe);
+            bool bNoLLVMMCExe = !File.Exists(pathToLLvmExe);
             if (bNoLLVMMCExe)
             {
-                string sFilesNotFound = "";
-                if (bNoLLVMMCExe)
-                    sFilesNotFound += "/files/llvm-mc.exe ";
-                string sMsg = string.Format(format: "Required file not found! ({0})", arg0: sFilesNotFound.Trim());
+                string sMsg = string.Format(format: "Required file not found! ({0})", arg0: pathToLLvmExe);
                 this.Hide();
                 RadMessageBox.Show(this, sMsg, caption: "Error", buttons: MessageBoxButtons.OK);
                 this._CanRun = false;
             }
         }
 
-        private void radButton1_Click(object sender, EventArgs e)
+        private void convertToAsm_Click(object sender, EventArgs e)
         {
-            if (radRadioButton1.IsChecked)
-            {
-                aParams = "-arch=arm";
+            string input = asmToHexAsmTextbox.Text;
+            if (writeToInputFile(input)) {
+                executeCommand(false, new receivedConsoleResponseDelegate(
+                    (output) => {
+                        string converted = getHexValuesFromConsoleOutput(output);
+                        SetTextboxText(asmToHexHexTextBox, converted);
+                }));
+            } else {
+                // ErrorHandling
             }
-            else if (radRadioButton2.IsChecked)
+        }
+        private void convertToHex_Click(object sender, EventArgs e)
+        {
+            string input = hexToAsmHexTextbox.Text;
+            if (writeToInputFile(input))
             {
-                aParams = "-arch=thumb";
+                executeCommand(true, new receivedConsoleResponseDelegate(
+                    (output) => {
+                        string converted = getHexValuesFromConsoleOutput(output);
+                        SetTextboxText(asmToHexHexTextBox, converted);
+                    }));
             }
-            else if (radRadioButton3.IsChecked)
-            {
-                aParams = "-arch=aarch64";
+            else {
+                // ErrorHandling
             }
-            else if (radRadioButton4.IsChecked)
-            {
-                aParams = "-arch=x86";
-            }
-            else if (radRadioButton5.IsChecked)
-            {
-                aParams = "-arch=x86-64";
-            }
-
         }
 
-        private void radButton2_Click(object sender, EventArgs e)
+        private string getHexValuesFromConsoleOutput(string output)
         {
-            if (radRadioButton1.IsChecked)
-            {
-                aParams = "-arch=arm";
+            string converted = "";
+
+            int idx = output.IndexOf("[");
+            if (idx > 0){
+                string tmp = output.Substring(idx + 1);
+                idx = tmp.IndexOf("]");
+                if (idx > 0)
+                {
+                    tmp = tmp.Substring(0, idx);
+                    string tmpValue = "";
+                    int val = -1;
+                    while (tmp.Length > 0)
+                    {
+                        int commaIdx = tmp.IndexOf(",");
+                        if (commaIdx > 0) {
+                            tmpValue = tmp.Substring(0, commaIdx);
+                        }
+                        // parse hex to int
+                        val = Convert.ToInt32(tmpValue, 16);
+                        converted += val.ToString("X2") + " ";
+
+                        if (commaIdx > 0) {
+                            tmp = tmp.Substring(commaIdx + 1);
+                        } else {
+                            tmp = "";
+                        }
+                    }
+                }
             }
-            else if (radRadioButton2.IsChecked)
+
+            return converted.Trim();
+        }
+
+        private bool writeToInputFile(string inputText)
+        {
+            try {
+                StreamWriter sw = new StreamWriter(_PathInputFile);
+                sw.Write(inputText);
+                sw.Flush();
+                sw.Close();
+            } catch (Exception ex)
             {
-                aParams = "-arch=thumb";
+                Console.WriteLine(ex.Message);
+                return false;
             }
-            else if (radRadioButton3.IsChecked)
+
+            return true;
+        }
+        private void executeCommand(bool isDisassemble, receivedConsoleResponseDelegate del)
+        {
+            Process p = new Process();
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.FileName = _PathLLVMMCexe;
+            p.StartInfo.Arguments = string.Format(_LlvmExecuteCommand, _PathInputFile, getSelectedParam());
+            if (isDisassemble) {
+                p.StartInfo.Arguments += " -disassemble";
+            }
+            consoleResponse = "";
+            p.OutputDataReceived += new DataReceivedEventHandler(
+                (s, e) =>
+                {
+                    if (e.Data == null)
+                    {
+                        del(consoleResponse);
+                    } else
+                    {
+                        consoleResponse += e.Data + "\n";
+                    }
+                }
+            );
+            p.ErrorDataReceived += new DataReceivedEventHandler((s, e) => {
+                del(e.Data);
+                Console.WriteLine(e.Data);
+            });
+
+            p.Start();
+            p.BeginOutputReadLine();
+        }
+
+        public void SetTextboxText(RadTextBox textBox, string text)
+        {
+            if (InvokeRequired)
             {
-                aParams = "-arch=aarch64";
+                this.Invoke(new Action<RadTextBox,string>(SetTextboxText), new object[] { textBox, text });
+                return;
             }
-            else if (radRadioButton4.IsChecked)
+            textBox.Text = text;
+        }
+        private string getSelectedParam()
+        {
+            string param = "";
+            if (radioArm.IsChecked)
             {
-                aParams = "-arch=x86";
+                param = "-arch=arm";
             }
-            else if (radRadioButton5.IsChecked)
+            else if (radioThumb.IsChecked)
             {
-                aParams = "-arch=x86-64";
+                param = "-arch=thumb";
             }
+            else if (radioAarch64.IsChecked)
+            {
+                param = "-arch=aarch64";
+            }
+            else if (radioX86.IsChecked)
+            {
+                param = "-arch=x86";
+            }
+            else if (radioX86_64.IsChecked)
+            {
+                param = "-arch=x86-64";
+            }
+            return param;
         }
 
         private void radButton3_Click(object sender, EventArgs e)
