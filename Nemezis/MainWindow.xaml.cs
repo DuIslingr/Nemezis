8a8e7fdcf46ffb3cb0f05459a46953752e060ad5
Unknown <alcatraz0890@live.com>
2017-03-19 21:34:56 -0400

    Update
    Added ability to press enter key in respective asm/hex text box to then perform the convert operation without having to click the button
    
    Added {} to radio selection to fix compile errors.
    
    Updated Material and MahApps nuget packages to latest stable versions.
    
    Fixed minor UI issues.

--- Nemezis/MainWindow.xaml.cs
+++ Nemezis/MainWindow.xaml.cs
@@ -6,119 +6,148 @@ using System.Windows;
 using System.Windows.Controls;
 using MahApps.Metro.Controls;
 using System.Text.RegularExpressions;
-
+using System.Windows.Input;
 
 namespace Nemizisv2
 {
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : MetroWindow
-{
-public bool _CanRun = true;
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public bool _CanRun = true;
 
-private bool lldbGDB1WasClicked = false;
+        private bool lldbGDB1WasClicked = false;
 
-private string _FilesFolderPath = AppDomain.CurrentDomain.BaseDirectory + "files";
-private string _PathLLVMMCexe {
-        get { return Path.Combine(_FilesFolderPath, "llvm-mc.exe"); }
-}
-private string _PathInputFile {
-        get { return Path.Combine(_FilesFolderPath, "tmpFile"); }
-}
-
-private string _LlvmExecuteCommand = " \"{0}\" {1} -show-encoding";
-
-private delegate void ReceivedConsoleResponseDelegate(string output);
-
-private string consoleResponse = "";
-
-public MainWindow()
-{
-        InitializeComponent();
-
-
-        InitializeComponent();
-
-        string localUserPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-
-        CheckFiles();
-        ShowInTaskbar = true;
-}
-
-private void CheckFiles()
-{
-        string pathToLLvmExe = Path.Combine(_FilesFolderPath, _PathLLVMMCexe);
-        bool bNoLLVMMCExe = !File.Exists(pathToLLvmExe);
-        if (bNoLLVMMCExe)
+        private string _FilesFolderPath = AppDomain.CurrentDomain.BaseDirectory + "files";
+        private string _PathLLVMMCexe
         {
+            get { return Path.Combine(_FilesFolderPath, "llvm-mc.exe"); }
+        }
+        private string _PathInputFile
+        {
+            get { return Path.Combine(_FilesFolderPath, "tmpFile"); }
+        }
+
+        private string _LlvmExecuteCommand = " \"{0}\" {1} -show-encoding";
+
+        private delegate void ReceivedConsoleResponseDelegate(string output);
+
+        private string consoleResponse = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            InitializeComponent();
+
+            string localUserPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+            CheckFiles();
+            ShowInTaskbar = true;
+        }
+
+        private void CheckFiles()
+        {
+            string pathToLLvmExe = Path.Combine(_FilesFolderPath, _PathLLVMMCexe);
+            bool bNoLLVMMCExe = !File.Exists(pathToLLvmExe);
+            if (bNoLLVMMCExe)
+            {
                 string sMsg = string.Format(format: "Required file not found! ({0})", arg0: pathToLLvmExe);
                 this.Hide();
                 MessageBox.Show(this, sMsg, caption: "Error", button: MessageBoxButton.OK);
                 this._CanRun = false;
                 Environment.Exit(1);
+            }
         }
-}
-
-private void convertAsmToHex_Click_1(object sender, RoutedEventArgs e)
-{
-        lldbGDB1WasClicked = true;
-        string input = ASM2HEX_ASMBX.Text;
-        if (writeToInputFile(input))
+        private void ConvertASM()
         {
+            lldbGDB1WasClicked = true;
+            string input = ASM2HEX_ASMBX.Text;
+            if (writeToInputFile(input))
+            {
                 executeCommand(false, new ReceivedConsoleResponseDelegate(
-                                       (output) => {
-                                        List<string> converted = getHexValuesFromConsoleOutput(output);
-                                        string finalString = string.Join(" ", converted);
-                                        SetTextboxText(ASM2HEX_HEXBX, finalString);
-                                }));
-        }
-        else
-        {
+                                       (output) =>
+                                       {
+                                           List<string> converted = getHexValuesFromConsoleOutput(output);
+                                           string finalString = string.Join(" ", converted);
+                                           SetTextboxText(ASM2HEX_HEXBX, finalString);
+                                       }));
+            }
+            else
+            {
                 // ErrorHandling
+            }
         }
-}
 
-private void convertHexToAsm_Click_1(object sender, RoutedEventArgs e)
-{
-        string input = HEX2ASM_HEXBX.Text;
-        string[] hexvals = input.Split(' ');
-        if (hexvals.Length <= 0)
+        private void OnKeyDownHandler1(object sender, KeyEventArgs e)
         {
-                return;
+            if (e.Key == Key.Return)
+            {
+                ConvertASM();
+            }
         }
-        string fileString = "";
-        foreach (string val in hexvals)
+
+        private void OnKeyDownHandler2(object sender, KeyEventArgs e)
         {
+            if (e.Key == Key.Return)
+            {
+                ConvertHex();
+            }
+        }
+
+        private void convertAsmToHex_Click_1(object sender, RoutedEventArgs e)
+        {
+            ConvertASM();
+        }
+
+        private void ConvertHex()
+        {
+            string input = HEX2ASM_HEXBX.Text;
+            string[] hexvals = input.Split(' ');
+            if (hexvals.Length <= 0)
+            {
+                return;
+            }
+            string fileString = "";
+            foreach (string val in hexvals)
+            {
                 string fixedVal = val.Replace("0x", "");
                 fixedVal = "0x" + fixedVal.ToUpper();
                 fileString += fixedVal + " ";
-        }
-        fileString = fileString.Trim();
+            }
+            fileString = fileString.Trim();
 
-        if (writeToInputFile(fileString))
-        {
+            if (writeToInputFile(fileString))
+            {
                 executeCommand(true, new ReceivedConsoleResponseDelegate(
-                                       (output) => {
-                                        string command = getAsmCommandFromConsoleOutput(output);
-                                        SetTextboxText(HEX2ASM_ASMBX, command);
-                                }));
-        }
-        else
-        {
+                                       (output) =>
+                                       {
+                                           string command = getAsmCommandFromConsoleOutput(output);
+                                           SetTextboxText(HEX2ASM_ASMBX, command);
+                                       }));
+            }
+            else
+            {
                 // ErrorHandling
+            }
         }
-}
 
-private string getAsmCommandFromConsoleOutput(string output)
-{
-        string returnVal = "";
-        string[] lines = output.Split('\n');
-        string encodingEnd = "encoding:";
-        Regex reg = new Regex("\t.* encoding:");
-        Match match = reg.Match(output);
-        if (match.Success)
+        private void convertHexToAsm_Click_1(object sender, RoutedEventArgs e)
         {
+            ConvertHex();
+        }
+
+        private string getAsmCommandFromConsoleOutput(string output)
+        {
+            string returnVal = "";
+            string[] lines = output.Split('\n');
+            string encodingEnd = "encoding:";
+            Regex reg = new Regex("\t.* encoding:");
+            Match match = reg.Match(output);
+            if (match.Success)
+            {
                 string line = match.Value.Replace(encodingEnd, "");
 
                 string cmd = line.Trim();
@@ -126,150 +155,171 @@ private string getAsmCommandFromConsoleOutput(string output)
                 cmd = cmd.Replace("\t", " ");
                 cmd = cmd.Trim();
                 returnVal = cmd;
-        }
+            }
 
-        return returnVal;
-}
-private List<string> getHexValuesFromConsoleOutput(string output)
-{
-        List<string> values = new List<string>();
-        Regex reg = new Regex("\\[[0-9a-fx, ]+\\]");
-        Match match = reg.Match(output);
-        if (match.Success)
+            return returnVal;
+        }
+        private List<string> getHexValuesFromConsoleOutput(string output)
         {
+            List<string> values = new List<string>();
+            Regex reg = new Regex("\\[[0-9a-fx, ]+\\]");
+            Match match = reg.Match(output);
+            if (match.Success)
+            {
                 char[] charsToTrim = { '[', ']' };
                 string fixedMatch = match.Value.Trim(charsToTrim);
                 string[] parts = fixedMatch.Split(',');
                 foreach (string part in parts)
                 {
-                        string fixedPart = part.ToUpper().Replace("0X", "");
-                        values.Add(fixedPart);
+                    string fixedPart = part.ToUpper().Replace("0X", "");
+                    values.Add(fixedPart);
                 }
+            }
+
+            return values;
         }
 
-        return values;
-}
-
-private bool writeToInputFile(string inputText)
-{
-        try
+        private bool writeToInputFile(string inputText)
         {
+            try
+            {
                 StreamWriter sw = new StreamWriter(_PathInputFile);
                 sw.Write(inputText);
                 sw.Flush();
                 sw.Close();
-        }
-        catch (Exception ex)
-        {
+            }
+            catch (Exception ex)
+            {
                 Console.WriteLine(ex.Message);
                 return false;
+            }
+
+            return true;
         }
-
-        return true;
-}
-private void executeCommand(bool isDisassemble, ReceivedConsoleResponseDelegate del)
-{
-        Process p = new Process();
-        p.StartInfo.RedirectStandardError = true;
-        p.StartInfo.RedirectStandardOutput = true;
-        p.StartInfo.UseShellExecute = false;
-        p.StartInfo.CreateNoWindow = true;
-        p.StartInfo.FileName = _PathLLVMMCexe;
-        p.StartInfo.Arguments = string.Format(_LlvmExecuteCommand, _PathInputFile, getSelectedParam());
-
-        if (isDisassemble)
+        private void executeCommand(bool isDisassemble, ReceivedConsoleResponseDelegate del)
         {
+            Process p = new Process();
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.FileName = _PathLLVMMCexe;
+            p.StartInfo.Arguments = string.Format(_LlvmExecuteCommand, _PathInputFile, getSelectedParam());
+
+            if (isDisassemble)
+            {
                 p.StartInfo.Arguments += " -disassemble";
-        }
-        consoleResponse = "";
-        p.OutputDataReceived += new DataReceivedEventHandler(
-                (s, e) =>
-                        {
+            }
+            consoleResponse = "";
+            p.OutputDataReceived += new DataReceivedEventHandler(
+                    (s, e) =>
+                            {
                                 if (e.Data == null)
                                 {
-                                        del(consoleResponse);
+                                    del(consoleResponse);
                                 }
                                 else
                                 {
-                                        consoleResponse += e.Data + "\n";
+                                    consoleResponse += e.Data + "\n";
                                 }
-                        }
-                );
-        p.ErrorDataReceived += new DataReceivedEventHandler((s, e) => {
-                                del(e.Data);
-                                Console.WriteLine(e.Data);
-                        });
+                            }
+                    );
+            p.ErrorDataReceived += new DataReceivedEventHandler((s, e) =>
+            {
+                del(e.Data);
+                Console.WriteLine(e.Data);
+            });
 
-        p.Start();
-        p.BeginOutputReadLine();
-}
+            p.Start();
+            p.BeginOutputReadLine();
+        }
 
-public void SetTextboxText(TextBox textBox, string text)
-{
-        if (!CheckAccess())
+        public void SetTextboxText(TextBox textBox, string text)
         {
+            if (!CheckAccess())
+            {
                 Dispatcher.Invoke(new Action<TextBox, string>(SetTextboxText), new object[] { textBox, text });
                 return;
+            }
+            textBox.Text = text;
         }
-        textBox.Text = text;
-}
 
-private string getSelectedParam()
-{
-        string param = "";
-        if (radioArm.IsChecked == true)
+        private string getSelectedParam()
         {
+            string param = "";
+            if (radioArm.IsChecked == true)
+            {
                 if (toggleButton.IsChecked == true && lldbGDB1WasClicked)
-                        param = "-triple=armv7eb";
-                lldbGDB1WasClicked = false;
+                {
+                    param = "-triple=armv7eb";
+                    lldbGDB1WasClicked = false;
+                }
                 else
-                        param = "-triple=armv7";
-                lldbGDB1WasClicked = false;
-        }
-        else if (radioThumb.IsChecked == true)
-        {
+                {
+                    param = "-triple=armv7";
+                    lldbGDB1WasClicked = false;
+                }
+            }
+            else if (radioThumb.IsChecked == true)
+            {
                 if (toggleButton.IsChecked == true && lldbGDB1WasClicked)
-                        param = "-triple=thumbv7eb";
-                lldbGDB1WasClicked = false;
+                {
+                    param = "-triple=thumbv7eb";
+                    lldbGDB1WasClicked = false;
+                }
                 else
-                        param = "-triple=thumbv7";
-                lldbGDB1WasClicked = false;
-        }
-        else if (radioAarch64.IsChecked == true)
-        {
+                {
+                    param = "-triple=thumbv7";
+                    lldbGDB1WasClicked = false;
+                }
+            }
+            else if (radioAarch64.IsChecked == true)
+            {
                 if (toggleButton.IsChecked == true && lldbGDB1WasClicked)
-                        param = "-triple=aarch64_be";
-                lldbGDB1WasClicked = false;
-                MessageBox.Show("Supposedly Big Endian is supported, but it seems to be the same hex. So just disable LLDB/GDB for arm64");
+                {
+                    param = "-triple=aarch64_be";
+                    lldbGDB1WasClicked = false;
+                    MessageBox.Show("Supposedly Big Endian is supported, but it seems to be the same hex. So just disable LLDB/GDB for arm64");
+                }
                 else
-                        param = "-triple=aarch64";
-                lldbGDB1WasClicked = false;
-        }
-        else if (radioX86.IsChecked == true)
-        {
+                {
+                    param = "-triple=aarch64";
+                    lldbGDB1WasClicked = false;
+                }
+            }
+            else if (radioX86.IsChecked == true)
+            {
                 if (toggleButton.IsChecked == true && lldbGDB1WasClicked)
-                        MessageBox.Show("Big Endian aka GDB/LLDB Mode is not supported with this Architecture");
-                lldbGDB1WasClicked = false;
+                {
+                    MessageBox.Show("Big Endian aka GDB/LLDB Mode is not supported with this Architecture");
+                    lldbGDB1WasClicked = false;
+                }
                 else
-                        param = "-arch=x86";
-                lldbGDB1WasClicked = false;
-        }
-        else if (radioX86_64.IsChecked == true)
-        {
+                {
+                    param = "-arch=x86";
+                    lldbGDB1WasClicked = false;
+                }
+            }
+            else if (radioX86_64.IsChecked == true)
+            {
                 if (toggleButton.IsChecked == true && lldbGDB1WasClicked)
-                        MessageBox.Show("Big Endian aka GDB/LLDB Mode is not supported with this Architecture");
-                lldbGDB1WasClicked = false;
+                {
+                    MessageBox.Show("Big Endian aka GDB/LLDB Mode is not supported with this Architecture");
+                    lldbGDB1WasClicked = false;
+                }
                 else
-                        param = "-arch=x86-64";
-                lldbGDB1WasClicked = false;
+                {
+                    param = "-arch=x86-64";
+                    lldbGDB1WasClicked = false;
+                }
+            }
+            return param;
         }
-        return param;
-}
 
-private void button2_Click(object sender, RoutedEventArgs e)
-{
-        MessageBox.Show("Nemezis v1.0.0!\nCoded by Blizzard\nCoded and Designed by Du'Islingr", "About");
-}
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Nemezis v1.0.1!\nCoded by Blizzard\nCoded and Designed by Du'Islingr", "About");
+        }
 
-}
+    }
 }
